ce6c00d919195b38b5707af7e03791e655fc9cb7
Ross Lyall <ross.lyall@gmail.com>
2026-02-28 17:43:50 +0000

    Auto-commit: Updated files

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/lib', '<rootDir>/lambda'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: [
+    'lib/**/*.ts',
+    'lambda/**/*.ts',
+    '!**/*.d.ts',
+    '!**/node_modules/**'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
+};
